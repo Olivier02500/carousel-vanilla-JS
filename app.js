8998b5763909a69c9eea7ee49d4572c55b68c5dd
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-13 16:43:47 +0100

    first commit, not finish

---
+++ app.js
@@ -0,0 +1,12 @@
+let leftButton = document.getElementById('switch-Left');
+let rightButton = document.getElementById('switch-Right');
+let section = document.getElementById('corps');
+
+//style de section
+section.style.backgroundColor= 'red';
+section.style.display = 'flex';
+section.style.justifyContent='space-between';
+section.style.alignItems= 'center';
+//a retirer
+section.style.height = '200px';
+
